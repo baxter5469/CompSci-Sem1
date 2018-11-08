99fb7acfeeb0fd9f7dbc5ed50af7b672d07ea61d
Andrew Ault <andrew@baxter.tamz.us>
2018-11-08 10:02:28 -0500

    finished

---
+++ E617/E617.java
@@ -0,0 +1,14 @@
+public class E617
+{
+	public static void main(String[] args)
+	{
+        int a;
+        int b;
+        for (a=1; a<=10;a++) {
+            for (b=1; b<=10; b++) {
+                System.out.printf("%4d",a*b);
+            }
+            System.out.println();
+        }
+	}
+}
