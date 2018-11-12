e696d5c4d636adc5600d9a6a65ce2eeef1689876
Andrew Ault <andrew@baxter.tamz.us>
2018-11-12 13:10:37 -0500

    finished

--- e618/E618.java
+++ e618/E618.java
@@ -6,25 +6,34 @@ public class E618
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a Side Length: ");
         int usrIn = input.nextInt();
-        int counter = 0;
+        int counter = 1;
+        int counter1 = 1;
         for (int numLen = 0;numLen < usrIn; numLen++){
-            for (int numOf = 0;numOf<= usrIn; numOf++) {
+            for (int numOf = 0;numOf< usrIn; numOf++) {
                 System.out.print("*");
             }
             System.out.print(" ");
-            for (int numOf2 = 0;numOf2<= usrIn; numOf2++) {
+            for (int numOf2 = 0;numOf2 < usrIn; numOf2++) {
                 // check if its top or bottom
-                if (counter < 1 || counter == usrIn) {
+                if (counter == 1 || counter == usrIn) {
                     // for (int numOf1 = 0;numOf1 <= usrIn; numOf1++) {
                         System.out.print("*");
                     //}
                 }
                 // if not top/bottom print middle
                 else {
-                    System.out.print("*" + (usrIn - 2) + "*");
+                    if (counter1 == 1 || counter1 == usrIn) {
+                        System.out.print("*");
+                        counter1++;
+                    }                 
+                    else {
+                        System.out.print(" ");
+                        counter1++;
+                    }
                 }
             }
             counter++;
+            counter1 = 1;
             System.out.println("");
         }
     }
