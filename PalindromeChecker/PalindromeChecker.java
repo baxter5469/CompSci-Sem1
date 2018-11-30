b06db2047a2879d59a45c768952fd2deb1dfc291
Andrew Ault <andrew@baxter.tamz.us>
2018-11-30 13:23:05 -0500

    added nested loops

--- PalindromeChecker/PalindromeChecker.java
+++ PalindromeChecker/PalindromeChecker.java
@@ -5,18 +5,28 @@ public class PalindromeChecker
 	{
         Scanner input = new Scanner(System.in);
         int usrLngth = 1;
+        int palindrome_counter = 0;
         while (usrLngth > 0) {
         System.out.print("Enter a String: ");
         String usrIn = input.nextLine();
         usrLngth = usrIn.length();
-        String reverse = "";
-        for (int i = usrLngth - 1; i >= 0; i--) {
-            reverse = reverse + usrIn.charAt(i);
+        char reverse = 'b';
+        char reverse1 = 'b';
+        int lastIndex = usrLngth - 1;
+        for (int i = (usrLngth / 2) - 1; i >= 0; i--) {
+            reverse = usrIn.charAt(i);
+            reverse1 = usrIn.charAt(lastIndex - i);
+            if (reverse == reverse1) {
+                palindrome_counter++;
+            }
+            else {
+                palindrome_counter = 0;
+            }
         }
         if (usrLngth < 1) {
             System.out.println("Empty line read - Goodbye");
         }
-        else if (usrIn.equals(reverse)) {
+        else if (palindrome_counter > 1) {
             System.out.println("This is a palindrome.");
         }
         else {
